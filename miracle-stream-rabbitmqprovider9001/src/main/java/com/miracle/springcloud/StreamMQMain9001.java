6b54bce42a5cd11337446859dc489d449e1edbb7
zhuangjianming <79716169@qq.com>
2021-04-15 14:19:31 +0800

    去除依赖

--- miracle-stream-rabbitmqprovider9001/src/main/java/com/miracle/springcloud/StreamMQMain9001.java
+++ miracle-stream-rabbitmqprovider9001/src/main/java/com/miracle/springcloud/StreamMQMain9001.java
@@ -2,8 +2,10 @@ package com.miracle.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StreamMQMain9001 {
     public static void main(String[] args) {
         SpringApplication.run(StreamMQMain9001.class, args);
